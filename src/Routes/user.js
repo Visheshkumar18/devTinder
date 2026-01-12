50b600257ec7b73cbeec85b4211d2d894d97e179
Vishesh Kumar <mrvisheshk@gmail.com>
2026-01-12 12:00:17 +0530

    api data modified

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -70,7 +70,7 @@ userRouter.get("/feed", auth, async (req, res) => {
       { _id: { $ne: loggedInUser._id } },
     ],
   })
-    .select("firstName lastName about skills photoUrl")
+    .select("firstName lastName about skills photoUrl age gender")
     .skip(skip)
     .limit(limit);
   res.send(user);
